768f9451c98f1b5728d3ce3f084ca496fa651daa
gsmin02 <gsmin5202@naver.com>
2025-01-02 22:26:54 +0900

    Google Map Create

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as gmaps from '@googlemaps/js-api-loader';
+	const { Loader } = gmaps;
+	import { PUBLIC_GOOGLE_MAP_API_KEY } from '$env/static/public';
+
+	let mapElement: HTMLElement;
+
+	onMount(async function () {
+		const loader = new Loader({
+			apiKey: PUBLIC_GOOGLE_MAP_API_KEY,
+			version: 'weekly'
+		});
+
+		const { Map } = await loader.importLibrary('maps');
+
+		new Map(mapElement, {
+			center: { lat: -34.397, lng: 150.644 },
+			zoom: 8
+		});
+	});
+</script>
+
+<div bind:this={mapElement} style="height: 100%;"></div>
